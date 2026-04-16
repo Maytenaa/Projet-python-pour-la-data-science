027bec6c06324c91abf962d1b0ad5c51b29fb9c4
Abelcornetcarlos <abelcornetcarlos@gmail.com>
2026-04-16 10:29:34 +0000

    rajout des autres communes ave une station de métro

--- src/get_data.py
+++ src/get_data.py
@@ -40,7 +40,11 @@ def fetch_dvf_api():
         
         df_rennes_list = []
         for chunk in chunks:
-            filtered_chunk = chunk[chunk['code_commune'].astype(str).str.contains('35238')].copy()
+            # On définit la liste des codes communes souhaités
+            codes_recherche = ['35238', '35051', '35281']
+
+            # On filtre le chunk
+            filtered_chunk = chunk[chunk['code_commune'].astype(str).isin(codes_recherche)].copy()
             df_rennes_list.append(filtered_chunk)
             
         df_rennes = pd.concat(df_rennes_list)
